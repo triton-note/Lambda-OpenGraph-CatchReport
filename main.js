1e527de487c91f8610fd0780501d1c74e7878dc9
Kunio Sawatani <devel@fathens.org>
2015-09-14 16:08:46 +0900

    与えられた情報を info プロパティにそのまま含めて返す
    API Gateway 側では JSON.stringify が出来ないので base64 でエンコードされたものも含めておきます。

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ var log = function() {
 exports.handler = function(event, context) {
     log('Received event:', event);
 
-	var appId = event.appId;
-	var appName = event.appName;
     var region = event.region;
     var table_report = event.table_report;
     var table_catch = event.table_catch;
@@ -72,9 +70,13 @@ exports.handler = function(event, context) {
     			 next(null, report);
     		 },
     		 function(report, next) {
+    			 log("Report: ", report);
+    			 
+    			 var base64 = new Buffer(JSON.stringify(event)).toString('base64');
+    			 
     			 next(null, {
-    				 "appId": appId,
-    				 "appName": appName,
+    				 "info": event,
+    				 "info_base64": base64,
     				 "report": report
     			 });
     		 }
